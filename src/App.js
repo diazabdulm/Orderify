d5b7616539fd2e848e6d0f4c315a7406f7a2488c
UFC205 <ufc205@gmail.com>
2019-11-30 19:46:40 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>ORDERIFY</h1>
+      <Navbar />
+      Coming Soon...
     </div>
   );
 }
