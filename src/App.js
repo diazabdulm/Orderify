05d535f059e45c8bc252c04c98bd78d0c8c0adf4
UFC205 <ufc205@gmail.com>
2019-12-02 19:44:54 -0400

    testing background samples

--- src/App.js
+++ src/App.js
@@ -3,34 +3,35 @@ import Grid from '@material-ui/core/Grid';
 import Navbar from './components/Navbar';
 import ProductListing from './components/ProductListing';
 import products from './sampledata';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Grid
-        container
-        spacing={2}
-        direction="row"
-        justify="space-evenly"
-        alignItems="center"
-        style={{ width: '95%', margin: '20px auto' }}
-      >
-        <Grid item xs={12} sm={6} lg={3}>
-          <ProductListing product={products[0]} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <ProductListing product={products[1]} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <ProductListing product={products[0]} />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <ProductListing product={products[1]} />
-        </Grid>
+  <div className='test'>
+    <Navbar />
+    <Grid
+      container
+      spacing={2}
+      direction="row"
+      justify="space-evenly"
+      alignItems="center"
+      style={{ width: '95%', margin: '20px auto' }}
+    >
+      <Grid item xs={12} sm={6} lg={3}>
+        <ProductListing product={products[0]} />
       </Grid>
-    </div>
-  );
+      <Grid item xs={12} sm={6} lg={3}>
+        <ProductListing product={products[1]} />
+      </Grid>
+      <Grid item xs={12} sm={6} lg={3}>
+        <ProductListing product={products[0]} />
+      </Grid>
+      <Grid item xs={12} sm={6} lg={3}>
+        <ProductListing product={products[1]} />
+      </Grid>
+    </Grid>
+  </div>
+);
 }
 
 export default App;
